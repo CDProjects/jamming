537f9dca9a5d37c22fd527baffd1a84be35791b4
CDProjects <caseydentprojects@gmail.com>
2023-12-16 19:27:46 +0200

    Component to render each track

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Track from '../Track/Track';
 import './TrackList.css';
 
-function TrackList({ tracks, isRemoval }) {
+function TrackList({ tracks }) {
   return (
     <div className="TrackList">
-      {
-        tracks.map(track => <Track key={track.id} track={track} isRemoval={isRemoval} />)
-      }
+      {tracks.map(track => <Track key={track.id} track={track} />)}
     </div>
   );
 }
