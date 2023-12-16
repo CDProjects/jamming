101ba827ca0ea043defc0a2bac4511eccd857bba
CDProjects <caseydentprojects@gmail.com>
2023-12-16 20:02:02 +0200

    PlayList comp, PlayList Name/Track props

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,17 +2,10 @@ import React from 'react';
 import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
-function Playlist() {
-  // Mock data
-  const playlistTracks = [
-    { id: 3, name: 'Track 3', artist: 'Artist 3', album: 'Album 3' },
-    { id: 4, name: 'Track 4', artist: 'Artist 4', album: 'Album 4' }
-    // Add more mock tracks
-  ];
-
+function Playlist({ playlistName, playlistTracks }) {
   return (
     <div className="Playlist">
-      <input defaultValue={'New Playlist'} />
+      <input defaultValue={playlistName} />
       <TrackList tracks={playlistTracks} isRemoval={true} />
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
