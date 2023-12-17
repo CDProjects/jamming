a1f9adf0894a201e0a66bcb2d5401fb7cc7b40f6
CDProjects <caseydentprojects@gmail.com>
2023-12-17 14:05:28 +0200

    Add tracks to playlist if not there already

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
-import SearchBar from './SearchBar/SearchBar';
-import SearchResults from './SearchResults/SearchResults';
-import Playlist from './Playlist/Playlist';
-import './App.css';
+import React, { useState } from "react";
+import SearchBar from "./SearchBar/SearchBar";
+import SearchResults from "./SearchResults/SearchResults";
+import Playlist from "./Playlist/Playlist";
+import "./App.css";
 
 function App() {
-  // Existing code for searchResults
-
-  // Mock data for playlist
-  const [playlistName, setPlaylistName] = useState('My Playlist');
+  const [searchResults, setSearchResults] = useState([
+    // Existing mock search results
+  ]);
+  const [playlistName, setPlaylistName] = useState("My Playlist");
   const [playlistTracks, setPlaylistTracks] = useState([
-    { id: 3, name: 'Track Name 3', artist: 'Artist 3', album: 'Album 3' },
-    { id: 4, name: 'Track Name 4', artist: 'Artist 4', album: 'Album 4' },
+    { id: 3, name: "Track Name 3", artist: "Artist 3", album: "Album 3" },
+    { id: 4, name: "Track Name 4", artist: "Artist 4", album: "Album 4" },
     // Add more tracks as needed
   ]);
+
+  const addTrackToPlaylist = (track) => {
+    if (playlistTracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return; // Track already in playlist, do nothing
+    }
+    setPlaylistTracks([...playlistTracks, track]);
+  };
 
   return (
     <div className="App">
       <SearchBar />
       <div className="App-playlist">
-        <SearchResults searchResults={searchResults} />
-        <Playlist playlistName={playlistName} playlistTracks={playlistTracks} />
+        <SearchResults
+          searchResults={searchResults}
+          onAdd={addTrackToPlaylist}
+        />
+        <Playlist
+          playlistName={playlistName}
+          playlistTracks={playlistTracks}
+          onNameChange={setPlaylistName}
+        />
       </div>
     </div>
   );
