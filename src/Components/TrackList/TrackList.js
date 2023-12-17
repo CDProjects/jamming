ab813a4a6c00016140a5d133653f29638cd05760
CDProjects <caseydentprojects@gmail.com>
2023-12-17 14:27:39 +0200

    Pass onRemove to TrackList and each Track

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Track from '../Track/Track';
 import './TrackList.css';
 
-function TrackList({ tracks, onAdd }) {
+function TrackList({ tracks, onAdd, onRemove, isRemoval }) {
   return (
     <div className="TrackList">
-      {tracks.map(track => <Track key={track.id} track={track} onAdd={onAdd} />)}
+      {tracks.map(track => 
+        <Track 
+          key={track.id} 
+          track={track} 
+          onAdd={onAdd} 
+          onRemove={onRemove} 
+          isRemoval={isRemoval} 
+        />
+      )}
     </div>
   );
 }
