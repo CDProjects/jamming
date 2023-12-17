7f88249888a337c85c964cc4c94eefaec639f611
CDProjects <caseydentprojects@gmail.com>
2023-12-17 14:10:53 +0200

    onAdd function through to each track

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Track from '../Track/Track';
 import './TrackList.css';
 
-function TrackList({ tracks }) {
+function TrackList({ tracks, onAdd }) {
   return (
     <div className="TrackList">
-      {tracks.map(track => <Track key={track.id} track={track} />)}
+      {tracks.map(track => <Track key={track.id} track={track} onAdd={onAdd} />)}
     </div>
   );
 }
