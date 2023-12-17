ed17513adaabe94801c0a6270e4168a7e4aa3751
CDProjects <caseydentprojects@gmail.com>
2023-12-17 14:26:05 +0200

    Pass onRemove to TrackList and each Track

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,14 +2,15 @@ import React from 'react';
 import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
-function Playlist({ playlistName, playlistTracks }) {
+function Playlist({ playlistName, playlistTracks, onNameChange, onRemove }) {
   return (
     <div className="Playlist">
-      <input defaultValue={playlistName} />
-      <TrackList tracks={playlistTracks} isRemoval={true} />
+      <input defaultValue={playlistName} onChange={e => onNameChange(e.target.value)} />
+      <TrackList tracks={playlistTracks} onRemove={onRemove} isRemoval={true} />
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
   );
 }
 
 export default Playlist;
+
