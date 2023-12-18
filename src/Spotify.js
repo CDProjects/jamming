cd0d6949183fc7edd578e7659a70255e47cf7b58
CDProjects <caseydentprojects@gmail.com>
2023-12-18 20:28:23 +0200

    Spotify authentication and retrieval

---
+++ src/Spotify.js
@@ -0,0 +1,34 @@
+const clientId = 'Y_SPOTIFY_CLIENT_ID'; // Replace with my client ID from Spotify
+const redirectUri = 'http://localhost:3000/'; // Replace with my redirect URI
+
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    // Check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      // Clear the parameters from the URL, so the app doesn't try grabbing the access   token after it has expired
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  },
+
+  // Add other methods to interact with Spotify API here
+};
+
+export default Spotify;
