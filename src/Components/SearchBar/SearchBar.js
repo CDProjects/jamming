5051a1506e4f32fda1c4b683174ae0598bf0ed22
CDProjects <caseydentprojects@gmail.com>
2023-12-26 17:12:01 +0200

    Async calls updated

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
+import Spotify from "./Spotify"; // Path to Spotify.js
 import './SearchBar.css';
 
-function SearchBar({ onSearch }) {
+function SearchBar() {
   const [term, setTerm] = useState('');
 
   const handleTermChange = (event) => {
     setTerm(event.target.value);
   };
 
-  const handleSearch = () => {
-    onSearch(term);
+  const handleSearch = async () => {
+    try {
+      const results = await Spotify.search(term);
+      // Assuming you have a method to update the search results in your state
+      // updateSearchResults(results);
+    } catch (error) {
+      console.error("Error during Spotify search:", error);
+      // Handle the error, possibly update state with an error message
+    }
   };
 
   return (
